ca4c072aec2c4eee9656a65172bf5bff57a47dff
Àlex Serra <bounteous17@protonmail.com>
2022-09-15 11:50:57 +0200

    refactor: replace custom features with native ones

--- index.js
+++ index.js
@@ -13,13 +13,6 @@ module.exports = (options) => {
       throw new Error('config is required');
     }
 
-    const { keepAlive, keepAliveTimer: keepAliveTimerParam } = config;
-    if (keepAliveTimerParam && (isNaN(keepAliveTimerParam) || keepAliveTimerParam <= 0)) {
-      throw new Error(`${keepAliveTimerParam} is not a valid number.`);
-    }
-    // 30 seconds by default
-    const keepAliveTimer = keepAliveTimerParam ? keepAliveTimerParam * 1000 : 30000;
-
     ({ logger } = dependencies);
 
     if (!logger) {
@@ -28,26 +21,30 @@ module.exports = (options) => {
 
     // https://github.com/redis/node-redis#basic-example
     const host = config.url || config.host || '127.0.0.1';
-    const protocol = config.tls ? 'rediss://' : 'redis://';
-    logger.info(`Connecting to ${host} with protocol ${protocol} based on TLS config`);
+    const database = config.db || 0;
+    let protocol = 'redis://';
+    const socket = {
+      connectTimeout: 10000,
+      keepAlive: 5000,
+    };
+    if (config.tls) {
+      protocol = 'rediss://';
+      socket.tls = true;
+      socket.servername = host;
+    }
+    const url = protocol + host + ':' + config.port + '/' + database;
+    logger.info(`Connecting ${url}`);
     client = redis.createClient({
-      url: protocol + '@' + host + ':' + config.port,
+      url,
       password: config.password,
-  });
+      socket,
+    });
 
     if (config.no_ready_check) {
       connectToRedis();
     } else {
       await connectToRedis();
     }
-
-    /**
-     * Redis instances in Azure disconnect the client after an IDLE time causing a forced reconnection.
-     * https://gist.github.com/JonCole/925630df72be1351b21440625ff2671f#file-redis-bestpractices-node-js-md
-     */
-    keepAlive && setInterval(() => {
-      client.ping();
-    }, keepAliveTimer);
 
     return client;
   };
